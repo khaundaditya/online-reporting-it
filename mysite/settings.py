ef7869f70b2a83d9e4a914cfc105fc8b3197f4a8
khaundaditya <aditya.khaund51@gmail.com>
2017-05-31 15:11:17 +0530

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,17 +113,17 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME', ''),
-        'USER': os.environ.get('DATABASE_USER', ''),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
-        'HOST': os.environ.get('DATABASE_HOST', ''),
-        'PORT': os.environ.get('DATABASE_PORT', ''),
-    }
-    # 'default': dj_database_url.config(
-    #     default=config('DATABASE_URL')
-    # )
+    # 'default': {
+    #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get('DATABASE_NAME', ''),
+    #     'USER': os.environ.get('DATABASE_USER', ''),
+    #     'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+    #     'HOST': os.environ.get('DATABASE_HOST', ''),
+    #     'PORT': os.environ.get('DATABASE_PORT', ''),
+    # }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 
 }
 
